b66ba132db021a7a92f13ff2022576bc280d0f8c
gonzoibox <gonzoibox@gmail.com>
2019-05-09 19:04:58 +0300

    add socket.on

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const nunjucks = require('nunjucks');
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {serverClient: true});
+
 
 nunjucks.configure('./client/views', {
     autoescape: true,
@@ -12,6 +15,13 @@ app.use('/assets', express.static('./client/public'));
 app.get('/', (req, res) => {
     res.render('index.html', { date: new Date() });
 });
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+      console.log(data);
+    });
+  });
 
 app.listen(3000, '0.0.0.0', () => {
     console.log('Server started on 3000');
