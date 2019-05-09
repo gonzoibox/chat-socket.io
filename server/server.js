f14fdad383eba945bc8bbf8d9d499151de79d9f0
gonzoibox <gonzoibox@gmail.com>
2019-05-09 17:33:44 +0300

    set  path

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const path =require('path');
 
-app.listen(7777, '0.0.0.0', () => {
-    console.log('Server started on 7777');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server started on 3000');
 });
 
